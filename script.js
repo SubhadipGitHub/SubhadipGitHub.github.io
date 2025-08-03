81b5b6a34525858259cdb8b6e9b4760371dde3be
Subhadip Dutta <subhadip.dutta.18@gmail.com>
2025-08-04 01:20:58 +0530

    Update script.js
    updated my new skillset

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["CX Consultant", "Data Scientist", "Android Developer", "Game Developer"],
+        strings: ["Technical Lead", "Integration Specialist", "Fusion ERP/CX Consultant", "Data Scientist", "Android Developer", "Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["CX Consultant", "Data Scientist", "Android Developer", "Game Developer"],
+        strings: ["Technical Lead", "Integration Specialist", "Fusion ERP/CX Consultant", "Data Scientist", "Android Developer", "Game Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
